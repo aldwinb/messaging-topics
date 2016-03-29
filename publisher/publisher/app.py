23d234cdb71503e07329795dd17782d48858437e
Aldwin Barredo <aldwin.barredo@innroad.com>
2016-03-29 17:01:57 -0400

    Added support for parameterized number of messages to publish.

--- publisher/publisher/app.py
+++ publisher/publisher/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-import msgbus
 import configparser
 import datetime as dt
 import inspect
+import msgbus
 import os.path
+import sys
+import uuid
 
 def main():
     pre_path =os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -16,9 +18,13 @@ def main():
                                          exchange=exchange,
                                          topics=config['rabbitmq']['topics'])
 
-    message = "Hello, world!"
-    msgbus.RabbitMqClient.publish(message=message, options=opts)
-    print(str.format("[{0}] Sent message in topic {1}", dt.datetime.now().isoformat(' '), opts.topics))
+    num_msgs = 100
+    if len(sys.argv) > 1:
+        num_msgs = int(sys.argv[1])
+    for i in range(num_msgs):
+        message = str.format("This is a test message with id {0}", uuid.uuid4())
+        msgbus.RabbitMqClient.publish(message=message, options=opts)
+        print(str.format("[{0}] Sent message in topic {1}", dt.datetime.now().isoformat(' '), opts.topics))
 
 
 if __name__ == '__main__':
