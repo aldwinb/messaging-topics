639fc8bec5809d2a81cedc3f6e2e6230e6eff1a2
Aldwin Barredo <aldwin.barredo@innroad.com>
2016-03-29 13:29:48 -0400

    Added simulation script.

---
+++ simulation.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+die () {
+  echo >&2 "$@"
+  exit 1
+}
+
+# validate options
+#[ "$#" -eq "1" ] || die "Invalid argument(s). USAGE:"\
+#  "simulation.sh"
+
+# get current directory
+curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+exit_code=1
+
+# start external components
+if docker-compose -f $curr_dir/docker-compose.yml up -d; then
+
+  # if start was successful, run apps
+
+  # whatever happens, destroy external components
+  docker-compose -f $curr_dir/docker-compose.yml down -v
+  if [ "$exit_code" -eq "0" ]; then
+    exit_code=$?
+  fi
+fi
+
+exit $exit_code
