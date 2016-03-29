f6cdca5fa24ff81d9b8af769c93466a81d2803ad
Aldwin Barredo <aldwin.barredo@innroad.com>
2016-03-29 17:10:39 -0400

    Changed the format of printing messages received.

--- subscriber/subscriber/app.py
+++ subscriber/subscriber/app.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
 
-import msgbus
 import configparser
 import datetime as dt
 import inspect
+import msgbus
 import os.path
 
 def on_message(channel, method_frame, header_frame, body):
-    print(method_frame.delivery_tag)
-    print(body)
-    print()
+    print(str.format("[{0}] Received message: {1}",
+                     dt.datetime.now().isoformat(' '),
+                     body))
     channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
 
 def main():
-    pre_path =os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    pre_path =os.path.dirname(os.path.abspath(
+        inspect.getfile(inspect.currentframe())))
     config = configparser.ConfigParser()
     config.read(os.path.join(pre_path, 'app.ini'))
 
-    exchange = msgbus.RabbitMqExchange(name=config['rabbitmq']['exchange'], type='topic')
+    exchange = msgbus.RabbitMqExchange(
+        name=config['rabbitmq']['exchange'],
+        type='topic')
     opts = msgbus.RabbitMqChannelOptions(host=config['rabbitmq']['host'],
                                          exchange=exchange,
                                          topics=config['rabbitmq']['topics'],
@@ -27,7 +30,8 @@ def main():
     channel =  msgbus.RabbitMqClient.create_channel(opts)
     channel.basic_consume(on_message, opts.queue)
     try:
-        print (str.format("[{0}] Waiting for messages...", dt.datetime.now().isoformat(' ')))
+        print (str.format("[{0}] Waiting for messages...",
+                          dt.datetime.now().isoformat(' ')))
         channel.start_consuming()
     except KeyboardInterrupt:
         channel.stop_consuming()
