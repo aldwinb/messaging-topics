029782dafd7f4a3ee521d7ade7305d1bfa4e8431
Aldwin Barredo <aldwinb@users.noreply.github.com>
2016-04-06 17:21:47 -0400

    Moved the compose steps of publisher and subscriber to main compose file. Deleted references of publisher and subscriber compose files.

--- stop.sh
+++ stop.sh
@@ -8,8 +8,6 @@ network="pubsub"
 curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # destroy components
-docker-compose -f $curr_dir/publisher/docker-compose.yml stop
-docker-compose -f $curr_dir/subscriber/docker-compose.yml stop
 docker-compose -f $curr_dir/docker-compose.yml stop
 
 bash stop-elk.sh
